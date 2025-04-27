c27b9b2c886dbbd30265544016271f4ecb12c2b7
luka <kaj.lund@gmail.com>
2025-04-27 19:30:35 +0300

    Moved app related code from server to app

--- src/app.js
+++ src/app.js
@@ -3,46 +3,120 @@ import httpLogger from "pino-http";
 
 import { NotFoundError } from "./errors.js";
 import { errorHandler } from "./middleware/error-handler.js";
+import { getDB } from "./db.js";
+import { authUtil } from "./utils/auth.js";
 
-export function getApplication(options) {
-  const { cnf, log, db } = options;
-  const app = express();
-  const router = express.Router();
+import { getActivityServices } from "./api/activities/activity.service.js";
+import { getResourceServices } from "./api/resources/resource.service.js";
+import { getUserServices } from "./api/users/user.service.js";
 
-  const addGroups = (groups, prefix = "") => {
+import { getActivityHandler } from "./api/activities/activity.handler.js";
+import { getResourceHandler } from "./api/resources/resource.handler.js";
+import { getUserHandler } from "./api/users/user.handler.js";
+
+import { getRootRoutes } from "./api/root.routes.js";
+import { getActivityRoutes } from "./api/activities/activity.routes.js";
+import { getResourceRoutes } from "./api/resources/resource.routes.js";
+import { getUserRoutes } from "./api/users/user.routes.js";
+
+export class App {
+  constructor(cnf, log) {
+    this.cnf = cnf;
+    this.log = log;
+    this.app = express();
+    this.router = express.Router();
+    this.db = getDB(this.cnf, this.log);
+  }
+
+  #addGroups(groups, prefix = "") {
     groups.forEach(({ group, routes }) => {
       routes.forEach(({ method, path, middleware = [], handler }) => {
-        log.info(`Route: ${method} ${prefix}${group.prefix}${path}`);
-        router[method](prefix + group.prefix + path, [...(group.middleware || []), ...middleware], handler);
+        this.log.info(`Route: ${method} ${prefix}${group.prefix}${path}`);
+        this.router[method](prefix + group.prefix + path, [...(group.middleware || []), ...middleware], handler);
       });
     });
-  };
+  }
 
-  return {
-    app,
-    connectDB: async () => {
-      await db.connect();
-    },
-    disconnectDB: async () => {
-      await db.disconnect();
-    },
-    attachRoutes: (routes) => {
-      // Attach routes
-      addGroups(routes);
-      app.use(router);
-      // Add 404 handler
-      app.use((req, _res, next) => {
-        next(new NotFoundError(`Route ${req.originalUrl} was not found`));
-      });
-      // Add Generic Error handler
-      app.use(errorHandler);
-    },
-    setupMiddleware: () => {
-      app.disable("x-powered-by");
-      app.use(express.json());
-      if (cnf.LOG_HTTP) {
-        app.use(httpLogger({ logger: log }));
-      }
-    },
-  };
+  async #connectDB() {
+    await this.db.connect();
+  }
+  async #disconnectDB() {
+    await this.db.disconnect();
+  }
+
+  #setupMiddleware() {
+    this.app.disable("x-powered-by");
+    this.app.use(express.json());
+    if (this.cnf.LOG_HTTP) {
+      this.app.use(httpLogger({ logger: this.log }));
+    }
+  }
+
+  #attachRoutes(routes) {
+    this.#addGroups(routes);
+    this.app.use(this.router);
+    // Add 404 handler
+    this.app.use((req, _res, next) => {
+      next(new NotFoundError(`Route ${req.originalUrl} was not found`));
+    });
+
+    // Add Generic Error handler
+    this.app.use(errorHandler);
+  }
+
+  initialize() {
+    this.#setupMiddleware();
+
+    const authUtils = authUtil({ cnf: this.cnf, log: this.log });
+    // Initialize services
+    const svcActivity = getActivityServices({ log: this.log });
+    const svcResource = getResourceServices({ log: this.log });
+    const svcUser = getUserServices({ cnf: this.cnf, log: this.log, authUtils });
+
+    // Initialize handlers
+    const hndActivities = getActivityHandler({ svcActivity });
+    const hndResources = getResourceHandler({ svcResource });
+    const hndUsers = getUserHandler({ svcUser });
+    // Initialize routes
+
+    const rootRoutes = getRootRoutes();
+    const activityRoutes = getActivityRoutes({ hnd: hndActivities });
+    const resourceRoutes = getResourceRoutes({ hnd: hndResources });
+    const userRoutes = getUserRoutes({ hnd: hndUsers });
+
+    this.#attachRoutes([rootRoutes, activityRoutes, resourceRoutes, userRoutes]);
+  }
+
+  async start() {
+    this.log.info(`Environment: ${this.cnf.NODE_ENV}`);
+    this.log.info("Starting server...");
+    await this.#connectDB();
+    this.app.listen(this.cnf.PORT, () => {
+      this.log.info(`Server running on port ${this.cnf.PORT}`);
+    });
+  }
+
+  async stop() {
+    await this.#disconnectDB();
+    this.log.info("Server stopped");
+  }
+}
+
+export class AppBuilder {
+  #cnf;
+  #log;
+
+  withConfig(cnf) {
+    this.#cnf = cnf;
+    return this;
+  }
+
+  withLogger(log) {
+    this.#log = log;
+    return this;
+  }
+
+  build() {
+    return new App(this.#cnf, this.#log);
+  }
 }
--- src/server.js
+++ src/server.js
@@ -1,55 +1,22 @@
-import http from "node:http";
 import util from "node:util";
 
 import { getConfig } from "./config.js";
 import { getLogger } from "./logger.js";
-import { getDB } from "./db.js";
-import { getApplication } from "./app.js";
-import { authUtil } from "./utils/auth.js";
-
-import { getActivityServices } from "./api/activities/activity.service.js";
-import { getResourceServices } from "./api/resources/resource.service.js";
-import { getUserServices } from "./api/users/user.service.js";
-import { getActivityHandler } from "./api/activities/activity.handler.js";
-import { getResourceHandler } from "./api/resources/resource.handler.js";
-import { getUserHandler } from "./api/users/user.handler.js";
-import { getRootRoutes } from "./api/root.routes.js";
-import { getActivityRoutes } from "./api/activities/activity.routes.js";
-import { getResourceRoutes } from "./api/resources/resource.routes.js";
-import { getUserRoutes } from "./api/users/user.routes.js";
+import { AppBuilder } from "./app.js";
 
 const cnf = getConfig();
 const log = getLogger(cnf);
-const db = getDB(cnf, log);
-const authUtils = authUtil({ cnf, log });
-
-// Initialize services
-const svcActivity = getActivityServices({ log });
-const svcResource = getResourceServices({ log });
-const svcUser = getUserServices({ cnf, log, authUtils });
-// Initialize handlers
-const hndActivities = getActivityHandler({ svcActivity });
-const hndResources = getResourceHandler({ svcResource });
-const hndUsers = getUserHandler({ svcUser });
-// Initialize routes
-const rootRoutes = getRootRoutes();
-const activityRoutes = getActivityRoutes({ hnd: hndActivities });
-const resourceRoutes = getResourceRoutes({ hnd: hndResources });
-const userRoutes = getUserRoutes({ hnd: hndUsers });
-
-const application = getApplication({ cnf, log, db });
+const application = new AppBuilder().withConfig(cnf).withLogger(log).build();
 
 process.on("SIGINT", async () => {
   log.info("SIGINT signal received: closing HTTP server");
-  await application.disconnectDB();
-  log.info("Database connection closed");
+  await application.stop();
   process.exit(0);
 });
 
 process.on("SIGTERM", async () => {
   log.info("SIGTERM signal received: closing HTTP server");
-  await application.disconnectDB();
-  log.info("Database connection closed");
+  await application.stop();
   process.exit(0);
 });
 
@@ -63,15 +30,7 @@ process.on("unhandledRejection", (reason, p) => {
   process.exit(1);
 });
 
-log.info("Starting server...");
-log.info(`Environment: ${cnf.NODE_ENV}`);
+application.initialize();
+await application.start();
 
-application.setupMiddleware();
-application.attachRoutes([rootRoutes, activityRoutes, resourceRoutes, userRoutes]);
-await application.connectDB();
-
-const server = http.createServer(application.app).listen(cnf.PORT, () => {
-  log.info(`HTTP server running on port ${cnf.PORT}`);
-});
-
-export default server;
+export default application;
